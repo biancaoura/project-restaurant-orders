ef5af7b7c4eb95e330d3be3783196224102ef592
Bianca Oura <biancaoura@gmail.com>
2023-05-22 01:46:08 -0300

    feat: get main menu fn

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -24,6 +24,18 @@ class MenuBuilder:
 
         self.inventory.consume_recipe(curr_dish.recipe)
 
-    # Req 4
     def get_main_menu(self, restriction=None) -> pd.DataFrame:
-        pass
+        dishes = list(self.menu_data.dishes)
+
+        df = pd.DataFrame(
+                {
+                    "dish_name": dish.name,
+                    "ingredients": dish.get_ingredients(),
+                    "price": dish.price,
+                    "restrictions": dish.get_restrictions()
+                }
+                for dish in dishes
+                if restriction not in dish.get_restrictions()
+                and self.inventory.check_recipe_availability(dish.recipe)
+        )
+        return df
