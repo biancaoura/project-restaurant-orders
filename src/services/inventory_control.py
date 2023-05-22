2636fcd7a19768fa9b9ab24a7261a9316dd3d59e
Bianca Oura <biancaoura@gmail.com>
2023-05-22 01:45:40 -0300

    feat: check recipe availability fn

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -20,14 +20,18 @@ def read_csv_inventory(inventory_file_path=BASE_INVENTORY) -> Dict:
     return inventory
 
 
-# Req 5
 class InventoryMapping:
     def __init__(self, inventory_file_path=BASE_INVENTORY) -> None:
         self.inventory = read_csv_inventory(inventory_file_path)
 
-    # Req 5.1
     def check_recipe_availability(self, recipe: Recipe):
-        pass
+        for ingredient, amt in recipe.items():
+            if ingredient not in self.inventory:
+                return False
+            if amt > self.inventory[ingredient]:
+                return False
+
+        return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
